fefea7b1c556df78603320902a6ee334a9d5e3ac
robertquijano <quijanor41@gmail.com>
2023-10-21 10:44:30 +0800

    first repo

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MainComponent from './components/MainComponent.vue'
+
+</script>
+
+<template>
+  <div class="body">
+    <MainComponent />
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+}
+
+</style>
